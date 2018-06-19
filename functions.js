e417c76c38654f93b29f0b4ef7ed68436ccfabad
Paula <paulamadetzke@gmail.com>
2018-06-18 21:40:16 -0400

    new

--- functions.js
+++ functions.js
@@ -1,3 +1,10 @@
+/* Welcome to the magical world of helper functions!
+   All the parsing helper functions will be stored here.
+   Each function will come with a helpful tag for what it
+   does, what it returns, and a short tester to see how
+   it works. LET THE FUNCTIONS BEGIN!
+*/
+
 var endParen = function(string, stringLoc){
     var beginParen = 0;
     var endParen = 0;
@@ -23,17 +30,19 @@ var endParen = function(string, stringLoc){
     return(endLoc);
 }
 
+
 /*
-var stringy = "5*((1/(2+3))+sin(2*pi*(5+4))+7)";
+var stringy = "5*((1/23)+sin(2*pi*(5+4))+7)";
 
 console.log(stringy);
 for (var i = 0; i < stringy.length; i++){
     if (stringy[i] == '('){
-	console.log(stringy.slice(i, endParen(stringy, i)));
+	console.log("End paren at: " + endParen(stringy, i));
+	console.log(stringy.slice(i, endParen(stringy, i)+1));
     }
 }
-
 */
+
 
 var rmSpaces = function(string){
   var tempStr = "";
@@ -48,11 +57,26 @@ var rmSpaces = function(string){
 
 var slashToOver = function(string){
     var i = 0;
+    var d;
+    var offset;
+    
     for(i; i < string.length; i++){
 	if ( string[i] == '/'){
 	    if (string[i+1] != '('){
+		offset = 0;
+		d = i+1;
+		for(d; d < string.length; d++){
+		    if(!isNaN(string[d]) ){
+			offset++;
+		    }
+		    else {
+			break;
+		    }
+		}
+		offset++;
 		string = string.slice(0,i).concat('\\over{',
-		string.slice(i+1, i+2), '}', string.slice(i+2, string.length));
+		string.slice(i+1, i+offset), '}',
+		string.slice(i+offset, string.length));
 	    }
 	    else{
 		var endPar = endParen(string, i);
@@ -65,3 +89,7 @@ var slashToOver = function(string){
     return(string);
 }
 
+var stringy = "x/2 + y/4 + z/1333333336";
+stringy = rmSpaces(stringy);
+console.log(stringy);
+console.log(slashToOver(stringy));
